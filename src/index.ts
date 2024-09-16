3f52ffd078f49a523fcd11a9610fe578d84756c8
Felipe <lhipemachado@gmail.com>
2024-09-16 19:55:15 -0300

    commit inical

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import express, { json } from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req,res)=>{
+    res.json('ok')
+
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log('Servidor Inicializado')
+    
+})
